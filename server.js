23bbde0586183192e4bc716f5691c6052f89e936
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-11-19 02:52:21 +0530

    updated scripts

--- server.js
+++ server.js
@@ -1,24 +1,77 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const validUrl = require("valid-url");
 const app = express();
-
+const mongoose = require("mongoose");
 // Basic Configuration
 const port = process.env.PORT || 3000;
+//conn. mongoDb w/ mongoose
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((data) => console.log("DB CONNECTED"));
 
+// define schema
+let urlSchema = new mongoose.Schema({
+  originalUrl: String,
+  shortUrl: Number,
+});
+const Model = mongoose.model("Model", urlSchema);
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", async (req, res) => {
+  try {
+    let url = new URL(req.body.url);
+    let originalUrl = req.body.url;
+    let shortUrl = Math.floor(Math.random() * 1000);
+    console.log("req recvd, processing");
+
+    // saving url
+    let savedUrl = await new Model({
+      originalUrl,
+      shortUrl,
+    }).save();
+    console.log(savedUrl);
+
+    res.json({
+      original_url: originalUrl,
+      short_url: shortUrl,
+    });
+  } catch (e) {
+    // ! if invalid url
+    // ! typeerror is returned
+    // ! check instance of typeerrror
+    if (e instanceof TypeError)
+      res.json({
+        error: "invalid url",
+      });
+  }
+
+  //redirect url
+  app.get("/api/shorturl/:short", async (req, res) => {
+    let short = req.params.short;
+
+    let search = await Model.findOne({ shortUrl: short }).exec();
+    console.log("search rsult", search);
+    res.redirect(search.originalUrl);
+  });
+});
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
